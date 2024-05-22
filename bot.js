4fff95d55e25f709bee52d90042571bef49f64bd
Rostyslav <dg1h1t@protonmail.com>
2024-05-22 18:39:45 -0500

    restrictions on bot

--- bot.js
+++ bot.js
@@ -6,35 +6,45 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 // @znayka_gpt_bot bot name
 
+// Allowed group and user IDs from environment variables
+const allowedGroupId = process.env.ALLOWED_GROUP_ID;
+const allowedUserId = process.env.ALLOWED_USER_ID;
+
 // Handle /start command
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Ласкаво просимо до чат-бота OpenAI!');
+  if (msg.chat.id.toString() === allowedGroupId) {
+    bot.sendMessage(msg.chat.id, 'Ласкаво просимо до чат-бота OpenAI!');
+  }
 });
 
 // Handle /help command
 bot.onText(/\/help/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Команди:\n/start - Почати\n/help - Допомога\nПоставте питання, зазначивши @znayka_gpt_bot');
+  if (msg.chat.id.toString() === allowedGroupId) {
+    bot.sendMessage(msg.chat.id, 'Команди:\n/start - Почати\n/help - Допомога\nПоставте питання, зазначивши @znayka_gpt_bot');
+  }
 });
-
 
 // Handle messages mentioning the bot
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
+  const userId = msg.from.id;
   const text = msg.text;
+
+  // Ensure the message is from the allowed group or user
+  if (chatId.toString() !== allowedGroupId && userId.toString() !== allowedUserId) {
+    return;
+  }
 
   // Ensure text is defined before checking for mentions
   if (text && (text.includes('@znayka_gpt_bot') || text.includes('всезнайка'))) {
-    // console.log("processing...");
     bot.sendChatAction(chatId, 'typing');
 
     const query = text.replace('@znayka_gpt_bot', '').replace('всезнайка', '').trim();
     const prompt = `Відповідай українською мовою на наступне повідомлення: ${query}`;
 
     try {
-
       // Send a placeholder message
       const placeholderMessage = await bot.sendMessage(chatId, 'думаю 🤔...');
-      // console.log("chatId = ", chatId);
 
       const response = await axios.post('https://api.openai.com/v1/chat/completions', {
         model: "gpt-4o",
